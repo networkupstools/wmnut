468fafa0958a138cc10c2054580df2f4f414877a
Jim Klimov <jimklimov@gmail.com>
2023-09-01 11:26:15 +0200

    autogen.sh: add a sanity-check report for libupsclient version as seen by pkg-config
    Signed-off-by: Jim Klimov <jimklimov@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
+# Pre-configure step for WMNut
+
+if (command -v pkg-config) >/dev/null 2>/dev/null ; then
+    NUTVER="`pkg-config --modversion libupsclient`"
+    case "$NUTVER" in
+        [01].*|2.[01234567].*)
+            echo "WARNING: pkg-config finds older NUT ($NUTVER) libraries than required by this WMNut release. You may get build warnings, or have to pass custom ones with configure options!" >&2
+            echo "===========" >&2
+            ;;
+        "") echo "WARNING: pkg-config did not find NUT libraries. You may have to pass custom ones with configure options!" >&2
+            echo "===========" >&2
+            ;;
+    esac
+fi
+
 autoreconf -i
